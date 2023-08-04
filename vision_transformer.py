fa537f5799fe8ccf3f8d10060ea1a6a1449f49c8
lenbrocki <45183581+lenbrocki@users.noreply.github.com>
2023-08-04 10:37:30 +0200

    add return_no_mlp to Block.forward() method

--- vision_transformer.py
+++ vision_transformer.py
@@ -104,11 +104,13 @@ class Block(nn.Module):
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
-    def forward(self, x, return_attention=False):
+    def forward(self, x, return_attention=False, return_no_mlp=False):
         y, attn = self.attn(self.norm1(x))
         if return_attention:
             return attn
         x = x + self.drop_path(y)
+        if return_no_mlp:
+            return  x
         x = x + self.drop_path(self.mlp(self.norm2(x)))
         return x
 
